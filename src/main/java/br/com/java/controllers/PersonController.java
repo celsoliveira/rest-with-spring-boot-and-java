b35e870b57868496661cd335f22a7f11d270b19d
Celso de Oliveira Silva <celsoliveira@gmail.com>
2025-07-21 22:02:20 -0300

    Trabalhando com verbo GET e operações matemáticas

---
+++ src/main/java/br/com/java/controllers/PersonController.java
@@ -0,0 +1,9 @@
+package br.com.java.controllers;
+
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class PersonController {
+
+
+}
---
+++ src/main/java/br/com/java/services/PersonServices.java
@@ -0,0 +1,6 @@
+package br.com.java.services;
+
+public class PersonServices {
+
+
+}
